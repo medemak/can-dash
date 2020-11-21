0f11c34e698c63c84fde4b447b61bdf3b3e3ed08
kara <kara.medema@gmail.com>
2020-11-21 18:37:47 -0500

    Update Student.js

--- can-dash/src/components/Student.js
+++ can-dash/src/components/Student.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+
+
 class Student extends React.Component {
     constructor(props) {
         super(props);
@@ -13,9 +18,34 @@ class Student extends React.Component {
 
     render() {
         return (
-            <div class="student">
-                <h2>{this.state.name}</h2>
-                <p>Student ID: {this.state.id} Grade:{this.state.grade} Period: {this.state.period}</p>
+            <div className="student">
+                <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                        <Paper className='paper'>
+                            <h2>{this.state.name}</h2> 
+                            <img src="https://via.placeholder.com/300" alt="Chart Placeholder" />
+                            <p>
+                                Student ID: {this.state.id} <br />
+                                Grade:{this.state.grade} <br />
+                                Period: {this.state.period}
+                            </p>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper className='paper'>
+                            <h3>Scores</h3>
+                            <ul>
+                                <li>scores here</li>
+                            </ul>
+
+                            <h3>Can Do</h3>
+                            <ul>
+                                <li>sexy time</li>
+                            </ul>
+                        </Paper>
+                    </Grid>
+                </Grid>
+
             </div>
         );
     }
