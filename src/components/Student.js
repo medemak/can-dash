850000b220fe26f8a997a6c22c356ed34efb138f
Kara Medema <medemak@mysdhc.net>
2020-12-02 12:03:25 -0500

    updating ui

--- src/components/Student.js
+++ src/components/Student.js
@@ -10,29 +10,7 @@ import 'react-svg-radar-chart/build/css/index.css'
 
 
 class Student extends React.Component {
-    data = [
-        {
-            data: {
-              battery: 0.7,git-
-              design: .8,
-              useful: 0.9,
-              speed: 0.67,
-              weight: 0.8
-            },
-            meta: { color: 'blue' }
-          }
-    ];
-
-    captions = {
-        // columns
-        battery: 'Battery Capacity',
-        design: 'Design',
-        useful: 'Usefulness',
-        speed: 'Speed',
-        weight: 'Weight'
-      };
-
-    
+   
     constructor(props) {
         super(props);
         this.state = {
@@ -43,6 +21,58 @@ class Student extends React.Component {
             canDashScore:3
         };    
     }
+
+    maxScores = {
+        score1:500,
+        score2:500,
+        score3:2000,
+        score4:35,
+        score5:1
+    }  
+
+    scores = {
+            // columns
+            score1: 350,
+            score2: 340,
+            score3: 798,
+            score4: 28,
+            score5: 0
+
+    }
+
+    data = [
+        {
+            data: {
+              score1: this.scores.score1 / this.maxScores.score1,
+              score2:this.scores.score2 / this.maxScores.score2,
+              score3: this.scores.score3 / this.maxScores.score3,
+              score4: this.scores.score4 / this.maxScores.score4,
+              score5: this.scores.score5 / this.maxScores.score5
+            },
+            meta: { color: 'blue' }
+          }
+    ];
+
+   
+
+    captions = {
+        // columns
+        score1: 'FSA-R',
+        score2: 'FSA-W',
+        score3: 'SAT',
+        score4: 'ACT',
+        score5: 'N/A'
+      };
+
+    options = {
+        scales: 5, // show scale circles?
+        captionProps: () => ({
+            className: 'caption',
+            textAnchor: 'middle',
+            fontSize: 16,
+            fontFamily: 'sans-serif'
+        }),
+      }
 
     render() {
         return (
@@ -56,6 +86,7 @@ class Student extends React.Component {
                                 captions={this.captions}
                                 data={this.data}
                                 size={450}
+                                options={this.options}
                             />
                             <p>
                                 Student ID: {this.state.id} <br />
@@ -69,7 +100,11 @@ class Student extends React.Component {
                         <Paper className='paper'>
                             <h3>Scores</h3>
                             <ul>
-                                <li>scores here</li>
+                                <li><strong>{this.captions.score1}</strong>: {this.scores.score1}</li>
+                                <li><strong>{this.captions.score2}</strong>: {this.scores.score2}</li>
+                                <li><strong>{this.captions.score3}</strong>: {this.scores.score3}</li>
+                                <li><strong>{this.captions.score4}</strong>: {this.scores.score4}</li>
+                                <li><strong>{this.captions.score5}</strong>: {this.scores.score5}</li>
                             </ul>
 
                             <h3>Can Do</h3>
@@ -78,8 +113,8 @@ class Student extends React.Component {
                             </ul>
                         </Paper>
                     </Grid>
-                    <Grid xs={4}>
-                        <Paper>
+                    <Grid item xs={4}>
+                        <Paper className="paper">
                             <h3>Recommendations</h3>
                             <ul>
                                 <li>List</li>
