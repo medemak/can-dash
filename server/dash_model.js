9121368bf9ab8bcc8da38a235f08c06b1f2c86ed
kara <kara.medema@gmail.com>
2020-12-05 11:50:20 -0500

    fixed!

--- server/dash_model.js
+++ server/dash_model.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user:'can-dash',
+    user:'postgres',
     host:`localhost`,
     database:'can-dash',
     password:'root',
@@ -9,40 +9,44 @@ const pool = new Pool({
 });
 
 const getStudents = () => {
-    return new Promise(function(resolve, reject) {
-      pool.query('SELECT * FROM Student ORDER BY last_name DESC', (error, results) => {
-        if (error) {
-          reject(error) 
-        }
-        resolve(results.rows); 
-      })
-    }) 
-  }
-  const createStudent = (body) => {
-    return new Promise(function (resolve, reject) {
-      const { firstName,lastName } = body;
-      pool.query('INSERT INTO Student (first_name,last_name) VALUES ($1, $2) RETURNING *', [firstName,lastName], (error, results) => {
-        if (error) {
-          reject(error);
-        }
-        resolve(`A new Student has been added added: ${JSON.stringify(results.rows[0])}`);
-      });
-    });
-  }
-  const deleteStudent = (studentId) => {
-    return new Promise(function(resolve, reject) {
-      const id = parseInt(studentId)
-      pool.query('DELETE FROM Student WHERE id = $1', [id], (error, results) => {
-        if (error) {
-          reject(error) 
-        } 
-        resolve(`Student deleted with ID: ${id}`)
-      })
-    }) 
-  }
-  
-  module.exports = {
-    getStudents,
-    createStudent,
-    deleteStudent,
-  }
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM Student ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const createStudent = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { name, email } = body
+
+    pool.query('INSERT INTO Student (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new Student has been added added: ${JSON.stringify(results.rows[0])}`)
+    })
+  })
+}
+
+const deleteStudent = (StudentId) => {
+  return new Promise(function(resolve, reject) {
+    const id = parseInt(StudentId)
+
+    pool.query('DELETE FROM Student WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`Student deleted with ID: ${id}`)
+    })
+  })
+}
+
+module.exports = {
+  getStudents,
+  createStudent,
+  deleteStudent,
+}
--- server/index.js
+++ server/index.js
@@ -1,30 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3001
-
-
 const dash_model = require('./dash_model')
-
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', `http://localhost:3000`);
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Access-Control-Allow-Headers');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
 
-// Get students
 app.get('/', (req, res) => {
   dash_model.getStudents()
   .then(response => {
     res.status(200).send(response);
   })
-.catch(error => {
-  res.status(500).send(error);
+  .catch(error => {
+    res.status(500).send(error);
   })
 })
 
-app.post('/students',(req, res) => {
+app.post('/students', (req, res) => {
   dash_model.createStudent(req.body)
   .then(response => {
     res.status(200).send(response);
@@ -34,7 +30,7 @@ app.post('/students',(req, res) => {
   })
 })
 
-app.delete('/students/:id',(req,res) => {
+app.delete('/students/:id', (req, res) => {
   dash_model.deleteStudent(req.params.id)
   .then(response => {
     res.status(200).send(response);
@@ -46,4 +42,4 @@ app.delete('/students/:id',(req,res) => {
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
-})
+}) 
